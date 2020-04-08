ce8f5bf5ad8d3e53aa59627060ac51924a17abf7
Diego Balseiro <dbalseiro@stackbuilders.com>
2020-04-08 15:12:31 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ms-dbo-mongo
+
+
+
+## Pre Requisites
+
+* Install [stack]
+
+## Setup and installation
+
+* clone this repo and build the project
+
+```
+$ git clone
+$ cd ms-dbo-mongo
+$ stack build
+```
+
+## Running tests
+
+```
+$ stack test
+```
---
+++ test/Spec.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Lib (app)
+import Test.Hspec
+import Test.Hspec.Wai
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = with (return app) $
+  describe "GET /healthcheck" $
+    it "responds with 200" $
+      get "/healthcheck" `shouldRespondWith` 200
