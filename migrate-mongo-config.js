39c2971ea0f3b8077fb4733daa56c678093f0a31
Diego Balseiro <dbalseiro@stackbuilders.com>
2020-04-14 16:48:15 -0500

    Migration can connect to azure

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -1,10 +1,22 @@
-const port = 27017;
+const port = process.env.DB_PORT;
 const host = process.env.DB_HOST;
 const name = process.env.DB_NAME;
 
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const auth = process.env.DB_AUTH === "true" ? `${user}:${pass}@` : "";
+
+const appName = process.env.DB_APPNAME;
+const ssl = process.env.DB_SSL === "true" ? `?ssl=true&appName=@${appName}@` : "";
+
+const trace = a => {
+  console.log(a);
+  return a;
+};
+
 const config = {
   mongodb: {
-    url: `mongodb://${host}:${port}`,
+    url: trace(`mongodb://${auth}${host}:${port}/${ssl}`),
     databaseName: name,
 
     options: {
